e705a356768b0f9a9368b9542139de848304e68f
Luciano <luciano.krebs@hotmail.com>
2017-05-20 12:13:09 -0300

    Jquery project

--- Learn JavaScript/05 javascript-and-jquery/javascript-and-dom.js
+++ Learn JavaScript/05 javascript-and-jquery/javascript-and-dom.js
@@ -1,11 +1,12 @@
 function main() {
-	$(".skillset").hide();
-  $(".skillset").fadeIn(2500);
-  $(".projects").hide();
-  $(".projects-button").on('click', function() {
-    $(".projects").toggle();
+  $('.skillset').hide();
+  $('.skillset').fadeIn(2000);
+  $('.projects').hide();
+  $('.projects-button').on('click', function() {
+		$(this).next().slideToggle(400);
     $(this).toggleClass('active');
-  });
+    $(this).text('Projects Viewed');
+	});
 }
 
 $(document).ready(main);
